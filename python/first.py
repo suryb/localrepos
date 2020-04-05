5637bc219da3dc08925fcd832dd1df33d1b5be62
sikijs <siki.js@gaeaglobal.com>
2020-04-04 20:22:26 -0700

    First py script..

---
+++ python/first.py
@@ -0,0 +1,12 @@
+print(" Hello world")
+print(" The world is beseiged by Corona virus. Version " + str(19))
+
+a = 20
+b = 20.3
+print (a*b)
+
+c = 209.222
+
+print (str(type(c)) + str(type(a)) + str(type(b)))
+
+
